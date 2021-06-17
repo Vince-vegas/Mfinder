97ca0ce88ef16bd9b5c20789e71a2c3f9cdf8fc6
vince vegas <sanchezvince769@gmail.com>
2021-06-18 01:51:40 +0800

    main page

--- src/Views/Main.jsx
+++ src/Views/Main.jsx
@@ -4,31 +4,31 @@ import '../Styles/main-page.scss';
 
 const Main = () => {
   return (
-    <div className="mn-homepage">
-      <div className="container">
-        <div className="row align-center justify-center hp-row">
-          <div className="hp-discover mb50">
-            <Link to="/home" className="btn hp-btn">
+    <div className='mn-homepage'>
+      <div className='container'>
+        <div className='row align-center justify-center hp-row'>
+          <div className='hp-discover mb50'>
+            <Link to='/home' className='btn hp-btn'>
               Discover Movies
             </Link>
           </div>
-          <div className="hp-content">
+          <div className='hp-content'>
             <p>
               Discover the latest and top movies worldwide. This website uses
               <a
-                href="https://developers.themoviedb.org/3"
-                className="link"
-                target="blank"
+                href='https://developers.themoviedb.org/3'
+                className='link'
+                target='blank'
               >
                 {' '}
                 The Movie DataBase API
               </a>{' '}
-              for retrieving movie data to this site. Its goal is to provide
+              for retrieving movies and collection. It's goal is to provide the
               users to browse all upcoming and exciting movies for free.{' '}
               <a
-                href="https://developers.themoviedb.org/3"
-                className="link"
-                target="blank"
+                href='https://developers.themoviedb.org/3'
+                className='link'
+                target='blank'
               >
                 Learn more.
               </a>
