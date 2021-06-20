a264524d2d64131f7e05ff6af3a28aef5235e941
vince vegas <sanchezvince769@gmail.com>
2021-06-21 06:18:51 +0800

    page

--- src/Components/Footer.jsx
+++ src/Components/Footer.jsx
@@ -5,17 +5,15 @@ import ReactDOM from 'react-dom';
 // footer
 const Footer = () => {
   return ReactDOM.createPortal(
-    <div className="footer">
-      <div className="container">
-        <div className="row justify-center">
-          <div className="col-md-6">
-            <div className="ft-content">
-              <div className="ft-img">
-                <img src={'/images/movie-db.png'} alt="TMDB" />
+    <div className='footer'>
+      <div className='container'>
+        <div className='row justify-center'>
+          <div className='col-md-6'>
+            <div className='ft-content'>
+              <div className='ft-img'>
+                <img src={'/images/movie-db.png'} alt='TMDB' />
               </div>
-              <h1 className="footer-text">
-                Developed &amp; designed by Vince Tañan
-              </h1>
+              <h1 className='footer-text'>Developed by Vince S. Tañan</h1>
             </div>
           </div>
         </div>
